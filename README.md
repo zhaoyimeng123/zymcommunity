2cf1f513ad46f9d54d35999c10853514efb6910e
tom_glb <tomcat_glb@atguigu.com>
2020-03-14 23:18:04 +0800

    second commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## 资料
+[github_OAuth](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
+
+
---
+++ src/main/java/com/zym/community/provider/GithubProvider.java
@@ -0,0 +1,53 @@
+package com.zym.community.provider;
+
+import com.alibaba.fastjson.JSON;
+import com.zym.community.dto.AccessTokenDTO;
+import com.zym.community.dto.GithubUser;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+
+
+import java.io.IOException;
+
+/**
+ * @author
+ * @date 2020/3/14-21:41
+ */
+@Component
+public class GithubProvider {
+
+    public String getAccessToken(AccessTokenDTO accessTokenDTO) {
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(JSON.toJSONString(accessTokenDTO),mediaType);
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();
+            String[] split = string.split("&");
+            String tokenStr = split[0];
+            String token = tokenStr.split("=")[1];
+            return token;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public GithubUser getUser(String accessToken){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token="+accessToken)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            return githubUser;
+        }catch (Exception e){
+        }
+        return null;
+    }
+}
